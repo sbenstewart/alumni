d514c02dfc4a5f81b371933579e898d2f0686bd7
Varadha Rajan <40229177+mrchaos10@users.noreply.github.com>
2018-07-15 13:54:46 +0530

    Add files via upload

--- login_module/registration.php
+++ login_module/registration.php
@@ -30,12 +30,74 @@ VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
         }
     }else{
 ?>
-<div class="form">
+    <style>
+input[type=text], select {
+    
+    width: 20%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+input[type=password], select {
+    width: 20%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+input[type=email], select {
+    width: 20%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+input[type=submit] {
+    width: 20%;
+    background-color: red;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #0067ab;
+}
+body  {
+    //background-image: url("https://media.istockphoto.com/photos/medicine-doctor-and-stethoscope-in-hand-touching-icon-medical-network-picture-id695349862");
+    background-color: black;
+    background-size: 1300px 780px;  
+}
+img.avatar {
+    width: 20%;
+    border-radius: 50%;
+}
+
+div {
+    border-radius: 5px;
+    padding: 20px;
+}
+</style>
+
+<div class="form" align="center">
 <h1>Registration</h1>
 <form name="registration" action="" method="post">
-<input type="text" name="username" placeholder="Username" required />
-<input type="email" name="email" placeholder="Email" required />
-<input type="password" name="password" placeholder="Password" required />
+    <img src="https://cdn4.vectorstock.com/i/thumb-large/74/83/avatar-man-medical-doctor-vector-11087483.jpg" alt="Avatar" class="avatar"><br>
+    <input type="text" name="username" placeholder="Username" required /><br>
+<input type="email" name="email" placeholder="Email" required /><br>
+<input type="password" name="password" placeholder="Password" required /><br>
 <input type="submit" name="submit" value="Register" />
 </form>
 </div>
